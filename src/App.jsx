3af9849ec9b90064fcc366d194a8b662b8589eab
amuthaselvi1993 <amuthaselvi1993@gmail.com>
2024-12-10 13:52:47 +0100

    my generic changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-function App() {}
+//SPA -Single page application
+//SSR - Server side rendering
+
+import image from "/react.png";
+import "./App.css";
+function App() {
+  return <img src={image} />;
+}
 
 export default App;
