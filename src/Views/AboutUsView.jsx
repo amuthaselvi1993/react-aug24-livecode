e1f7a20964904523e8fa5eb228d674cb3646796a
Endre Jordal <endre@jobloop.no>
2025-01-13 11:02:10 +0100

    routing

---
+++ src/Views/AboutUsView.jsx
@@ -0,0 +1,3 @@
+export default function AboutUsView() {
+  return <h1>Dette er About Us siden.</h1>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
+import HomeView from "./Views/HomeView.jsx";
+import AboutUsView from "./Views/AboutUsView.jsx";
+import NotFoundView from "./Views/NotFoundView.jsx";
+import UsersView from "./Views/UsersView.jsx";
+import UserView from "./Views/UserView.jsx";
+import HeaderMenu from "./Views/HeaderMenu.jsx";
 
+// SPA - Single Page Application
+// Routing.
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HeaderMenu />,
+    children: [
+      {
+        index: true,
+        element: <HomeView />,
+      },
+      {
+        path: "/about",
+        element: <AboutUsView />,
+      },
+      {
+        path: "*",
+        element: <NotFoundView />,
+      },
+      {
+        path: "/users",
+        element: <UsersView />,
+      },
+      {
+        path: "/users/:id",
+        element: <UserView />,
+      },
+    ],
+  },
+]);
+//http://localhost:5173/about
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
